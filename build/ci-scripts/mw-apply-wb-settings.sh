58190722530214bd3fbdaec1d9575c1e5cbe6a7e
Kim Pham <kim.pham@wikimedia.de>
2026-05-26 10:03:21 +0200

    sitelink change 6 test remove

--- build/ci-scripts/mw-apply-wb-settings.sh
+++ build/ci-scripts/mw-apply-wb-settings.sh
@@ -9,16 +9,15 @@ cd ../mediawiki
 function apply_client_settings {
   echo '$wgEnableWikibaseClient = true;' >> LocalSettings.php
   echo '$wgWBClientSettings["siteGlobalID"] = "enwiki";' >> LocalSettings.php
-  echo '$wgWBClientSettings["repoSiteId"] = "enwiki";' >> LocalSettings.php
-  echo '$wgWBClientSettings["siteGroup"] = "local";' >> LocalSettings.php
-  echo '$wgWBClientSettings["siteLinkGroups"] = [ "local" ];' >> LocalSettings.php
-  echo '$wgWBRepoSettings["siteLinkGroups"] = [ "local", "wikipedia" ];' >> LocalSettings.php
+  # echo '$wgWBClientSettings["repoSiteId"] = "enwiki";' >> LocalSettings.php
+  # echo '$wgWBClientSettings["siteGroup"] = "local";' >> LocalSettings.php
+  # echo '$wgWBClientSettings["siteLinkGroups"] = [ "local" ];' >> LocalSettings.php
   echo 'wfLoadExtension( "Scribunto" );' >> LocalSettings.php
 }
 
 function apply_repo_settings {
   echo '$wgEnableWikibaseRepo = true;' >> LocalSettings.php
-
+  echo '$wgWBRepoSettings["siteLinkGroups"] = [ "local", "wikipedia" ];' >> LocalSettings.php
   echo '$wgServer = "http://127.0.0.1:8080";' >> LocalSettings.php
   echo '$wgCanonicalServer = "http://127.0.0.1:8080";' >> LocalSettings.php
   echo '$wgScriptPath = "";' >> LocalSettings.php
@@ -58,15 +57,5 @@ else
   apply_repo_settings
   apply_client_settings
 fi
-
-# # Override siteGlobalID and repoSiteId for api testing
-# if [ "${E2E_SITELINK_SITE_ID:-}" = "default" ]; then
-#   echo '$wgWBClientSettings["siteGlobalID"] = "default";' >> LocalSettings.php
-#   echo '$wgWBClientSettings["repoSiteId"] = "default";' >> LocalSettings.php
-#   echo '$wgWBClientSettings["siteGroup"] = "local";' >> LocalSettings.php
-#   # REMOVE LATER KIM
-#   # echo '$wgWBClientSettings["siteLinkGroups"] = [ "local" ];' >> LocalSettings.php
-#   # echo '$wgWBRepoSettings["siteLinkGroups"] = [ "local", "wikipedia" ];' >> LocalSettings.php
-# fi
 
 apply_common_after_settings
